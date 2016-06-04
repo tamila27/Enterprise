4c0a74dbd923dcb7fde8d0d4df24ea1e8b293392
tamila <tamilaasanova@gmail.com>
2016-06-04 14:51:07 +0300

    module2 has been added

---
+++ src/com/goit/gojavaonline/module2/Runner.java
@@ -0,0 +1,35 @@
+package com.goit.gojavaonline.module2;
+
+/**
+ * Created by tamila on 6/2/16.
+ */
+public class Runner {
+    public static void main(String[] args){
+        Number[] intNumbers = new Integer[]{10, 20, 30};
+        intNumbers[2] = 10.48;
+        for (Number intNumber : intNumbers) {
+            System.out.println(intNumber);
+        }
+    }
+
+    /*public void test(List<Task<Integer>> intTasks) {
+        Executor<Number> numberExecutor = new ExecutorImpl<>();
+
+        for (Task<Integer> intTask : intTasks) {
+            numberExecutor.addTask(intTask);
+        }
+        numberExecutor.addTask(new LongTask(10L), new NumberValidator());
+
+        numberExecutor.execute();
+
+        System.out.println("Valid results:");
+        for (Number number : numberExecutor.getValidResults()) {
+            System.out.println(number);
+        }
+        System.out.println("Invalid results:");
+        for (Number number : numberExecutor.getInvalidResults()) {
+            System.out.println(number);
+        }
+    }*/
+
+}
---
+++ src/com/goit/gojavaonline/module2/Validator.java
@@ -0,0 +1,9 @@
+package com.goit.gojavaonline.module2;
+
+/**
+ * Created by tamila on 6/2/16.
+ */
+public interface Validator {
+    // Валидирует переданое значение
+    boolean isValid(Object result);
+}
